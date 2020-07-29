d424b62a9dee9fb28917c2e65c0278ff3f4e83d2
dpaniq <14brother@gmail.com>
2020-07-29 15:37:43 +0300

    Remove comments

--- static/js/canvas.js
+++ static/js/canvas.js
@@ -49,21 +49,8 @@ class Single {
         this.ctx.stroke()
     }
 
-    // Data Image
-
-    // setStartTime() {
-    //     this.paint.time.start = new Date().getTime()
-    // }
-
-    // setEndTime() {
-    //     this.paint.time.end = new Date().getTime() - this.paint.time.start
-    //     this.paint.time.start = 0 
-    // }
-
     // World
     async repeat (payload) {
-
-
         for (let i = 1; i < payload.length - 1; i++) {
             this.ctx.moveTo(payload[i].x_from, payload[i].y_from)
             this.ctx.lineTo(payload[i].x_moveTo, payload[i].y_moveTo)
@@ -71,45 +58,6 @@ class Single {
 
             await this.sleep(payload[i + 1].time - payload[i].time);
         }
-
-        // console.log('\nP', payload, typeof payload)
-        const startTime = new Date().getTime()
-        const endtime = payload[payload.length - 1].time
-        console.log(endtime)
-
-
-
-        // let currentTime = 0
-
-
-        // setInterval(() => {
-        // const timer = setInterval(() => {
-        //     const currentTime = new Date().getTime() - startTime
-        //     console.log(currentTime)
-
-        //     const current = payload.filter(line => {
-        //         line.time === currentTime
-        //         console.log(line.time, ' -!- ', currentTime, line.time === currentTime)
-        //     })
-        //     console.log(current)
-        //     //     if (currentTime === line.time) {
-        //     //         this.ctx.moveTo(line.x_from, line.y_from)
-        //     //         this.ctx.lineTo(line.x_moveTo, line.y_moveTo)
-        //     //         this.ctx.stroke()
-        //     //     }
-        //     // })
-
-        //     if (new Date().getTime() - startTime > endtime) clearInterval(timer)
-            
-           
-        // }, 1)
-
-        
-
-        
-
-        
-        
     }
 
     // ### TOOLS ###
@@ -130,15 +78,7 @@ btn.addEventListener('click', ev => {
 
 const socket = io();
 
-// console.log('Allright')
-// console.log(socket)
-
-// socket.on('msgToClient', payload => {
-//     console.log('PayLoad: ', payload)
-// })
-
 const solo = new Single('canvas_world')
 socket.on('msgToClient', payload => {
-    console.log('PayLoad: ', payload)
     solo.repeat(payload)
 })
